0f47f1903b5ed40bfc88d857c6d8f2bf2e0b017e
Josh Lyford <joshlyford@gmail.com>
2018-04-06 17:27:14 -0400

    update concurrency on runs

--- config/uptime-monitor.php
+++ config/uptime-monitor.php
@@ -75,7 +75,7 @@ return [
          * monitors concurrently. Set this to a lower value if you're getting weird errors
          * running the uptime check.
          */
-        'concurrent_checks' => 10,
+        'concurrent_checks' => 1,
 
         /*
          * The uptime check for a monitor will fail if the url does not respond after the
